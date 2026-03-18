653c4fdb2493178f90f97436b7894704d535cff6
qgedeak@gmail.com <qgedeak@gmail.com>
2026-03-18 07:57:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace trapezokCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trapez> trapezok = new List<Trapez>();
+            string[] fajlTartalom = File.ReadAllLines("trapezok.csv");
+
+            foreach (var sor in fajlTartalom)
+            {
+                trapezok.Add(new Trapez(sor));
+            }
+        }
+    }
+}
---
+++ Trapez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trapezokCLI
+{
+    public class Trapez
+    {
+        public Trapez(string egysor)
+        {
+            string[] sor = egysor.Split(" ");
+            a = int.Parse(sor[0]);
+            b = int.Parse(sor[1]);
+            c = int.Parse(sor[2]);
+            d = int.Parse(sor[3]);
+            m = int.Parse(sor[4]);
+        }
+
+        public int a { get; private set; }
+        public int b { get; private set; }
+        public int c { get; private set; }
+        public int d { get; private set; }
+        public int m { get; private set; }
+    }
+}
